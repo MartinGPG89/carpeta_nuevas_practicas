dea5491895fcd72acf3dc0df7ca6886b42282ccf
Martin Pereyra <martinbass1989@gmail.com>
2025-07-09 19:17:36 +0200

    primeros ejercicios

--- practicas_bucles.py
+++ practicas_bucles.py
@@ -1,3 +1,54 @@
+# 1️⃣ Muestra cada número multiplicado por 2
+'''numeros = [3, 7, 12, 5, 9]
+
+
+for i in numeros:
+    print(i*2, end=',')'''
+
+
+# 2️⃣ Imprime solo los números mayores que 6
+'''numeros = [3, 7, 12, 5, 9]
+
+for i in numeros:
+    if i > 6:
+        print(i)
+'''
+
+# 3️⃣ Suma todos los números de la lista y muestra el resultado final
+'''numeros = [3, 7, 12, 5, 9]
+suma = 0
+
+for i in numeros:
+    suma += i
+
+print(suma)
+'''
+
+
+# 4️⃣ Imprime la raíz cuadrada de cada número
+numeros = [1, 4, 9, 16, 25]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
